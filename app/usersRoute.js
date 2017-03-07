fc49f5eca3d85d11f6e0c05cc3d76f9845117482
SoriS <pashanikoluyk@mail.ru>
2017-03-07 14:20:01 +0200

    fix user routes

--- app/usersRoute.js
+++ app/usersRoute.js
@@ -48,14 +48,14 @@ module.exports = function (app) {
         console.log(currentUser.chats);
 
     });
+}
+// route middleware to make sure
+function isLoggedIn(req, res, next) {
 
-    // route middleware to make sure
-    function isLoggedIn(req, res, next) {
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
 
-        // if user is authenticated in the session, carry on
-        if (req.isAuthenticated())
-            return next();
-
-        // if they aren't redirect them to the home page
-        res.redirect('/');
-    }
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
